8ba02f016127979e306c98f64a4f49b1fc62a5e6
Hiếu Đoàn <hieu225@Hieus-MacBook-Pro-2.local>
2023-03-21 23:27:02 +0700

    fix bug and add condition for code chatgpt

--- app.py
+++ app.py
@@ -75,13 +75,14 @@ with block:
               temperature = gr.Slider(label="Độ sáng tạo của AI (tối thiểu 0, tối đa 1)", minimum=0, maximum=1, step=0.1, value=0.1, interactive=True)
             with gr.Column(scale=5, min_width=600):
               chatbot = gr.Chatbot(elem_id="chatbot_custom")
+              alert_gen_chatgpt = gr.Markdown(value="""<i style="color:#3ADF00"><center>Câu hỏi càng ngắn gọn số token càng nhỏ</center></i>""", visible=True) 
               message = gr.Textbox(placeholder="Hỏi chatgpt bất cứ vấn đề nào mà bạn muốn", label="Câu hỏi của bạn")
               state = gr.State()
               submit = gr.Button("Gửi câu hỏi")
               #submit gpt
-              submit.click(chat, inputs=[message, state, temperature, main_key], outputs=[chatbot, state])
+              submit.click(chat, inputs=[message, state, temperature, main_key], outputs=[chatbot, state, alert_gen_chatgpt])
               submit.click(lambda :"", None, message, scroll_to_output=True)
-              message.submit(chat, inputs=[message, state, temperature, main_key], outputs=[chatbot, state])
+              message.submit(chat, inputs=[message, state, temperature, main_key], outputs=[chatbot, state, alert_gen_chatgpt])
               message.submit(lambda :"", None, message, scroll_to_output=True)
               #clear history chat
               clear = gr.Button("Xóa lịch sử chat")
@@ -146,3 +147,4 @@ if AUTH == "False":
 else:
   block.queue(concurrency_count=1)
   block.launch(server_name = "0.0.0.0", auth = (ID,PASSWORD),debug = True)
+ 
--- chatgpt/app.py
+++ chatgpt/app.py
@@ -2,14 +2,13 @@ import os
 import openai
 from dotenv import load_dotenv
 load_dotenv('./.env')
-import requests
 import json
 # new code get response openai
 from typing import List, Tuple, Dict, Generator
 from langchain.llms import OpenAI
 import gradio as gr
 import requests
-
+import tiktoken
 #if you have OpenAI API key as an environment variable, enable the below
 #openai.api_key = os.getenv("OPENAI_API_KEY")
 
@@ -23,6 +22,18 @@ def create_history_messages(history: List[Tuple[str, str]]) -> List[dict]:
     history_messages = [{"role": "user", "content": m[0]} for m in history]
     history_messages.extend([{"role": "assistant", "content": m[1]} for m in history])
     return history_messages
+
+def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0301"):
+    encoding = tiktoken.encoding_for_model(model)
+    num_tokens = 0
+    for message in messages:
+        num_tokens += 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
+        for key, value in message.items():
+            num_tokens += len(encoding.encode(value))
+            if key == "name":  # if there's a name, the role is omitted
+                num_tokens += -1  # role is always required and always 1 token
+    num_tokens += 2  # every reply is primed with <im_start>assistant
+    return num_tokens
 
 def create_formatted_history(history_messages: List[dict]) -> List[Tuple[str, str]]:
     formatted_history = []
@@ -54,36 +65,53 @@ def chat(
     message: str, state: List[Dict[str, str]], temperature: float, api_key: str
 ) -> Generator[Tuple[List[Tuple[str, str]], List[Dict[str, str]]], None, None]:
     LLM = OpenAI(model_name=model_name, temperature=temperature, openai_api_key=api_key)
+    print("State 1: ",state)
     history_messages = state
     if history_messages == None:
         history_messages = []
         history_messages.append({"role": "system", "content": "A helpful assistant."})
 
-    history_messages.append({"role": "user", "content": message})
-    # We have no content for the assistant's response yet but we will update this:
-    history_messages.append({"role": "assistant", "content": ""})
+    # check max token
+    max_response_tokens = 256
+    token_limit= 4096
+    conversation = [{"role": "user", "content": message}]
+    conv_history_tokens = num_tokens_from_messages(conversation)
+    print("This token using with promt: ",conv_history_tokens)
+    if conv_history_tokens+max_response_tokens >= token_limit:
+        try:
+            formatted_history = create_formatted_history(state)
+            yield formatted_history, state, gr.update(value="""<i style="color:red"><center>AI không thể xử lý câu hỏi quá dài. Xin thử lại.</center></i>""", visible=True)
+        except:
+            yield [], [], gr.update(value="""<i style="color:red"><center>AI không thể xử lý câu hỏi quá dài. Xin thử lại.</center></i>""", visible=True)
+    else:
+        history_messages.append({"role": "user", "content": message})
+        # We have no content for the assistant's response yet but we will update this:
+        history_messages.append({"role": "assistant", "content": ""})
 
-    response_message = ""
+        response_message = ""
+        try:
+            chat_generator = LLM.client.create(
+                messages=history_messages, stream=True, model=model_name
+            )
+            # print("chat_generator", chat_generator)
+            for chunk in chat_generator:
+                if "choices" in chunk:
+                    for choice in chunk["choices"]:
+                        if "delta" in choice and "content" in choice["delta"]:
+                            new_token = choice["delta"]["content"]
+                            # Add the latest token:
+                            response_message += new_token
+                            # Update the assistant's response in our model:
+                            history_messages[-1]["content"] = response_message
 
-    chat_generator = LLM.client.create(
-        messages=history_messages, stream=True, model=model_name
-    )
-    print("State 1: ",state)
-    for chunk in chat_generator:
-        if "choices" in chunk:
-            for choice in chunk["choices"]:
-                if "delta" in choice and "content" in choice["delta"]:
-                    new_token = choice["delta"]["content"]
-                    # Add the latest token:
-                    response_message += new_token
-                    # Update the assistant's response in our model:
-                    history_messages[-1]["content"] = response_message
-
-                if "finish_reason" in choice and choice["finish_reason"] == "stop":
-                    break
-        formatted_history = create_formatted_history(history_messages)
-        yield formatted_history, history_messages
-
+                        if "finish_reason" in choice and choice["finish_reason"] == "stop":
+                            break
+                formatted_history = create_formatted_history(history_messages)
+                yield formatted_history, history_messages, gr.update(value=f"""<i style="color:#3ADF00"><center>Số token của câu hỏi: {num_tokens_from_messages(history_messages)+max_response_tokens}</center></i>""", visible=True)
+        except:
+            formatted_history = create_formatted_history(state)
+            yield formatted_history, state, gr.update(value="""<i style="color:red"><center>Có lỗi xảy ra. Có thể do tài khoản của bạn hoặc kết nối mạng.</center></i>""", visible=True)
+            pass
 
 def clear_history(state):
     return None
